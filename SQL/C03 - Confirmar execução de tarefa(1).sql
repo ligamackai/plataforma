16462814ac08d35f73279ad1bb013fff8b0df56c
Rafael Vidal 唐星原 <149404825+rafavidal1709@users.noreply.github.com>
2026-06-30 17:47:46 -0300

    Update C03 - Confirmar execução de tarefa(1).sql

--- SQL/C03 - Confirmar execução de tarefa(1).sql
+++ SQL/C03 - Confirmar execução de tarefa(1).sql
@@ -12,6 +12,15 @@ LANGUAGE plpgsql
 AS $procedure$
 
 BEGIN
+  -- Buscar o ID do grupo a partir da execução
+  SELECT o.grupo INTO v_grupo_id
+  FROM executou e
+  JOIN tarefa t ON t.id = e.tarefa
+  JOIN ocorreu o ON o.id = t.ocorrencia
+  WHERE e.id = in_executou_id;
+
+  -- Verificar permissão de coordenação para este grupo
+  PERFORM plataforma.verificar_permissao(in_executado_por, 'coordenação', v_grupo_id);
 
   IF in_horas < 0 THEN
     RAISE EXCEPTION 'A quantidade de horas nao pode ser negativa';
